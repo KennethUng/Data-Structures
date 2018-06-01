4ebb1b657eb1375e4ad368fbebb03a6c84f7d77d
Kenneth Ung <kennethleeung@gmail.com>
2018-06-01 09:56:42 -0700

    Created a file to test all of our Stack implementations

--- src/Stacks/StackTest.java
+++ src/Stacks/StackTest.java
@@ -1,4 +1,32 @@
 package Stacks;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class StackTest {
+
+    public static void main(String [] args) {
+        StackInterface<String> chainTest = new LinkedStack<String>();
+        System.out.println("Chained Link Stack Test");
+        addTo(chainTest);
+        System.out.println();
+        removeAndPrint(chainTest);
+        System.out.println("\n---------------------------");
+
+    }
+
+    public static void addTo(StackInterface stack) {
+        Random rd = new Random();
+        for(int i = 0; i < 10; i++) {
+            int random = rd.nextInt(100);
+            System.out.print(random + " ");
+            stack.push("" + random);
+        }
+    }
+
+    public static void removeAndPrint(StackInterface stack) {
+        while(!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
 }
